c51804563751aea87ee9ba59e43b5a766de2b07d
AnniAika <thierfelder.annika@gmail.com>
2019-01-13 17:04:29 +0100

    parameter dictionary added

--- saliency_model/itti_koch.py
+++ saliency_model/itti_koch.py
@@ -5,8 +5,9 @@ import scipy.signal as signal
 import matplotlib.image as mpimg
 from skimage import img_as_float64
 
-from .utils import *
-from .itti_koch_features import *
+# TODO change back!
+from utils import *
+from itti_koch_features import *
 
 
 class IttiKoch():
@@ -14,14 +15,25 @@ class IttiKoch():
     Itti and Koch main model class.
     Inputs upon init: path to the image, changes to setup dict
     '''
-    def __init__(self):
-        # TODO: add ( , dict): that can be used to set up features
+    def __init__(self, input_params):
         super().__init__()
-
-        self.mapwidth = 64
-        self.outer_sigma = [3,6]
-        self.inner_sigma = [1,1]
-        self.weights = [1,1,1]
+        
+        # dictionary with default params
+        self.params = {
+            "min_mapwidth": 64,
+            "num_center_scales": 3,
+            "outer_sigma": [3,6],
+            "inner_sigma": [1,1],
+            "topdown_weights": [1,1,1],
+            "gabor_theta": 4,
+            "gabor_sigma": 4,
+            "gabor_frequency": 0.1,
+            "gabor_phase": False
+        }
+        
+        # update the parameters with the input
+        for key in input_params:
+            self.params[key] = input_params[key]
 
         pass
 
@@ -32,9 +44,9 @@ class IttiKoch():
         returns a list of downsampled images with lenght according to inp scalars
         '''
         # determine size of center scales
-        self.mapsize = (round(img.shape[0] * (self.mapwidth / img.shape[1])), self.mapwidth)
+        self.params["mapsize"] = (round(img.shape[0] * (self.params["min_mapwidth"] / img.shape[1])), self.params["min_mapwidth"])
 
-        return downsample_image(img, self.mapsize[0], self.mapwidth, scalars)
+        return downsample_image(img, self.params["mapsize"][0], self.params["min_mapwidth"], scalars)
 
 
     def make_feature_maps(self, imgs, feature_func, *args):
@@ -45,7 +57,7 @@ class IttiKoch():
         '''
         feat_maps = feature_func(imgs, *args)
 
-        return convolve_receptive_field(feat_maps, self.inner_sigma, self.outer_sigma)
+        return convolve_receptive_field(feat_maps, self.params["inner_sigma"], self.params["outer_sigma"])
 
 
     def make_conspicuity_maps(self, img_scales, feature_func, *args):
@@ -55,11 +67,11 @@ class IttiKoch():
         conspicuity_maps = []
         for img in img_scales:
             feature_maps = self.make_feature_maps([img], feature_func, *args)
-            conspicuity_map = compute_conspicuity_map(feature_maps, self.mapsize)
+            conspicuity_map = compute_conspicuity_map(feature_maps, self.params["mapsize"])
             conspicuity_maps.append(conspicuity_map)
 
         # TODO sum & normalize across scales
-        conspicuity_map = compute_conspicuity_map(conspicuity_maps, self.mapsize)
+        conspicuity_map = compute_conspicuity_map(conspicuity_maps, self.params["mapsize"])
 
         return conspicuity_map
 
@@ -67,19 +79,31 @@ class IttiKoch():
         # TODO: add ( , keys): that will decide which feature to use
         '''Given an image returns its saliency'''
         img = img_as_float64(img) # convert to doubles if image is uint8
-        wj = self.weights
-
-        gabor_kernels = create_gabor_kernels()
 
         # compute spatial scales
-        img_scales = self.make_center_scales(img, [1, 2, 3])
+        print("Computing {} image scales".format(self.params["num_center_scales"]))
+        img_scales = self.make_center_scales(img, np.arange(self.params["num_center_scales"])+1)
 
         # compute conspicuity_map for each channel
+        # intensity
+        print("Computing saliency map for intensity.")
         intensity = self.make_conspicuity_maps(img_scales, compute_intensity)
+        
+        # color
+        print("Computing saliency map for color.")
         color = self.make_conspicuity_maps(img_scales, compute_color)
+        
+        # orientation
+        print("Creating Gabor kernels for orientation.")
+        gabor_kernels = create_gabor_kernels(theta = self.params["gabor_theta"],
+                                            sigma = self.params["gabor_sigma"],
+                                            frequency = self.params["gabor_frequency"],
+                                            phase = self.params["gabor_phase"])
+        print("Computing saliency map for orientation.")
         orientation = self.make_conspicuity_maps(img_scales, compute_orientation, gabor_kernels)
 
         # sum & normalize across channels
+        wj = self.params["topdown_weights"]
         saliency = wj[0]*intensity + wj[1]*color + wj[2]*orientation
 
         # return saliency

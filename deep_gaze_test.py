5bc5174b446aaa4ca8746ab60a02bf4c88b17543
Aleksandra Teska <aleksandra.teska@gmail.com>
2019-01-26 17:32:33 +0100

    models moved

--- deep_gaze_test.py
+++ deep_gaze_test.py
@@ -1,60 +1,17 @@
 import warnings
-warnings.simplefilter(action='ignore', category=FutureWarning)
-
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
-import seaborn as sns
-import tensorflow as tf
-import skimage
 import matplotlib.image as mpimg
-from scipy.misc import face
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
 from saliency_model.deep_gaze import run_deep_gaze
-sns.set_style('white')
 
-
-# def run_deep_gaze(img, model='DeepGaze'):
-    # ''' function runs DeepGaze or ICF models provided from DeepGazeII library, uses Matthias code '''
-    #
-    # image_data = img[np.newaxis, :, :, :]  # BHWC, three channels (RGB)
-    # centerbias_data = np.zeros((1, img.shape[0], img.shape[1], 1))  # BHWC, 1 channel (log density)
-    #
-    #
-    # if model == 'DeepGaze':
-    #     check_point = './deep_gaze/DeepGazeII.ckpt'  # DeepGaze II
-    # elif model == 'ICF':
-    #     check_point = './deep_gaze/ICF.ckpt'  # ICF
-    # else:
-    #     return  AssertionError('Wrong name of the model, could not be loaded! Use DeepGaze or ICF')
-    #
-    # tf.reset_default_graph()
-    # new_saver = tf.train.import_meta_graph('{}.meta'.format(check_point))
-    #
-    # # import deepgaze model
-    # input_tensor = tf.get_collection('input_tensor')[0]
-    # centerbias_tensor = tf.get_collection('centerbias_tensor')[0]
-    # log_density = tf.get_collection('log_density')[0]
-    # log_density_wo_centerbias = tf.get_collection('log_density_wo_centerbias')[0]
-    #
-    # with tf.Session() as sess:
-    #     new_saver.restore(sess, check_point)
-    #
-    #     log_density_prediction = sess.run(log_density, {
-    #         input_tensor: image_data,
-    #         centerbias_tensor: centerbias_data,
-    #     })
-    #
-    # smap = np.exp(log_density_prediction[0, :, :, 0])  # get dentisty predicion
-    # smap = smap / np.max(smap)   # normalize
-    #
-    # return smap, log_density_prediction
 
 # Load The Image
-
-path = './imgs/group.jpg'
+path = './imgs/lena.jpg'
 img = mpimg.imread(path)
 
-# account for
+# account for png files
 if np.shape(img)[2] == 4:
     img = img[:,:,0:3]
 
@@ -62,11 +19,19 @@ if np.shape(img)[2] == 4:
 smap, log_density_prediction = run_deep_gaze(img)
 smap_icf, log_density_prediction_icf = run_deep_gaze(img, model='ICF')
 
- # visualize the model
+# visualize the model
 fig, ax = plt.subplots(1, 2)
-ax[0].imshow(img, alpha=0.2)
-ax[0].imshow(smap, alpha=0.5)
+# ax[0].imshow(img, alpha=0.2)
+# ax[0].imshow(smap, alpha=0.5)
+# ax[1].imshow(img, alpha=0.2)
+# ax[1].imshow(smap_icf, alpha=0.5)
 
-ax[1].imshow(img, alpha=0.2)
-ax[1].imshow(smap_icf, alpha=0.5)
+print(type(smap))
+print(type(smap_icf))
+# print(smap.dtype())
+# print(smap_icf.dtype())
+
+ax[0].imshow(smap)
+ax[1].imshow(smap_icf)
+
 plt.show()
--- test_different_models.py
+++ test_different_models.py
@@ -1,25 +1,28 @@
 import os
+import warnings
+import skimage
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-import warnings
+from tqdm import tqdm
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
-import skimage
-from tqdm import tqdm
 import pysaliency
-import pysaliency.external_datasets
 from pysaliency.utils import MatlabOptions
 from saliency_model.itti_koch import IttiKoch
+from saliency_model.deep_gaze import run_deep_gaze
 
 plt.rcParams['image.cmap'] = 'gray'
 MatlabOptions.matlab_names = ['matlab', 'matlab.exe', '/usr/local/MATLAB/R2017b/bin/matlab']
 MatlabOptions.octave_names = []
 
 # Load The Image
-path = './imgs/balloons.png'
+path = './imgs/balloons.jpg'
 img = mpimg.imread(path)
-img = skimage.img_as_float(img)
+
+# account for png files
+if np.shape(img)[2] == 4:
+    img = img[:,:,0:3]
 
 # initiate our model
 IK = IttiKoch(verbose=False)
@@ -35,13 +38,22 @@ gbvs_ik = pysaliency.GBVSIttiKoch(location='test_models', cache_location=os.path
 # calculate saliency
 pbar = tqdm(total=100)
 
+# run deep gaze models
+smap_dg, log_density_prediction = run_deep_gaze(img)
+pbar.update(10)
+smap_icf, log_density_prediction_icf = run_deep_gaze(img, model='ICF')
+pbar.update(10)
+
+# run IK and faces
 saliency, _ = IK.run(img)
 pbar.update(10)
 saliency_face_only, _ = IK.run(img, keys = [], faces=True)
 pbar.update(10)
+
+# run pysaliency models
 smap_aim = aim.saliency_map(img)
-pbar.update(10)
 smap_sun = sun.saliency_map(img)
+pbar.update(10)
 pbar.update(10)
 smap_cas = cas.saliency_map(img)
 pbar.update(10)
@@ -54,25 +66,33 @@ pbar.update(10)
 
 pbar.close()
 
-fig, ax = plt.subplots(ncols=5, nrows=2, figsize=(20, 10))
+fig, ax = plt.subplots(ncols=6, nrows=2, figsize=(20, 10))
 ax[0][0].imshow(img)
 ax[0][1].imshow(saliency)
 ax[0][2].imshow(saliency_face_only)
-ax[0][3].imshow(-smap_sun)
-ax[0][4].imshow(-smap_aim)
+ax[0][3].imshow(smap_dg)
+ax[0][4].imshow(smap_icf)
+ax[0][5].imshow(-smap_sun)
 
-ax[1][0].imshow(smap_cas)
-ax[1][1].imshow(smap_gbvs)
-ax[1][2].imshow(smap_gbvs_ik)
-ax[1][3].imshow(smap_covsal)
+ax[1][0].imshow(-smap_aim)
+ax[1][1].imshow(smap_cas)
+ax[1][2].imshow(smap_gbvs)
+ax[1][3].imshow(smap_gbvs_ik)
 ax[1][4].imshow(smap_covsal)
+ax[1][5].imshow(smap_covsal)
 
+ax[0][0].set_title('original image')
 ax[0][1].set_title('IttiKoch_ours')
-ax[0][2].set_title('AIM')
-ax[0][3].set_title('SUN')
-ax[1][0].set_title('CAS')
-ax[1][1].set_title('GBVS')
-ax[1][2].set_title('GBVS_IK')
-ax[1][3].set_title('CovSal')
+ax[0][2].set_title('face detection')
+ax[0][3].set_title('DeepGazeII')
+ax[0][4].set_title('ICF (from DG)')
+ax[1][5].set_title('SUN')
+
+ax[1][0].set_title('AIM')
+ax[1][1].set_title('CAS')
+ax[1][2].set_title('GBVS')
+ax[1][3].set_title('GBVS_IK')
+ax[1][4].set_title('CovSal')
+ax[1][5].set_title('CovSal')
 
 plt.show()

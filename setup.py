5d83384d8536946ed5ec7492bff7491efb9602ba
Aleksandra Teska <aleksandra.teska@gmail.com>
2019-03-07 16:06:30 +0100

    setup.py added, installable saliency_model now

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='itti_koch',
+      version='0.1',
+      description='IttiKoch with face recognition model.',
+      url='https://github.com/alTeska/NISE_saliency',
+      author='Aleksandra Teska, Annika Thierfelder',
+      author_email='aleksandra.teska@gmail.com',
+      license='MIT',
+      packages=['saliency_model'],
+      zip_safe=False)
